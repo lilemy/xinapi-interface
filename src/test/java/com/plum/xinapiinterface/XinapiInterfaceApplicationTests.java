8024febe31f8d1df31843c59660bda6928845bb2
lilemy <2331845269@qq.com>
2024-03-28 14:40:28 +0800

    小新接口 开发模拟接口

---
+++ src/test/java/com/plum/xinapiinterface/XinapiInterfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plum.xinapiinterface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XinapiInterfaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
